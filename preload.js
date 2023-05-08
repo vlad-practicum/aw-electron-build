49e40243b142d6cdcdb0d25655d04001d377d164
vlad-practicum <129411371+vlad-practicum@users.noreply.github.com>
2023-05-08 19:43:45 +0300

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,25 @@
+/**
+ * The preload script runs before. It has access to web APIs
+ * as well as Electron's renderer process modules and some
+ * polyfilled Node.js functions.
+ *
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  const ipc = require('electron').ipcRenderer;
+  
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type]);
+  }
+
+  ipc.on('aw', function (e, msg) {
+    console.log(msg);
+  });
+});
